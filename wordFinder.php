0a7dffdaa068a3a2c52f22269224801ac2d7f33b
Mujtaba Rizvi <mujtaba.rizvi@arbisoft.com>
2024-06-28 13:36:48 +0500

    add wordFinder.php

---
+++ wordFinder.php
@@ -0,0 +1,19 @@
+<h1>Words Finder</h1>
+
+<form name="form" action="" method="get">
+    <input type="text" name="search" value="">
+</form>
+
+<h3>Similar Words:</h3>
+<ul>
+    <?php
+    require("words.php");
+    if (isset($_GET['search'])) {
+        $ans = $d1->getSimilarWords($_GET['search']);
+    }
+
+    foreach ($ans as $word) {
+        echo "<li>$word</li>";
+    }
+    ?>
+</ul>
